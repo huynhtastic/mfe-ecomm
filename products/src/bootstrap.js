5b207de0dc957d7a2fd398a28017507f88a24672
Richard Huynh <huynhtastic@gmail.com>
2021-09-01 17:15:46 -0500

    create fix for running isolation and in container

--- products/src/bootstrap.js
+++ products/src/bootstrap.js
@@ -1,10 +1,35 @@
 import faker from "faker";
 
-let products = "";
+const mount = (el) => {
+  let products = "";
 
-for (let i = 0; i < 5; i++) {
-  const name = faker.commerce.productName();
-  products += `<div>${name}</div>`;
+  for (let i = 0; i < 5; i++) {
+    const name = faker.commerce.productName();
+    products += `<div>${name}</div>`;
+  }
+
+  el.innerHTML = products;
+};
+
+// Context/Situation #1
+// running file in development in isolation
+// using local index.html
+// which has element with id='dev-products'
+// render app into element
+if (process.env.NODE_ENV === "development") {
+  const el = document.querySelector("#dev-products");
+
+  // assuming our contianer doesn't have an el w/ id='dev-products
+  if (el) {
+    // prob running in isolation
+    mount(el);
+  }
 }
 
-document.querySelector("#dev-products").innerHTML = products;
+// Context/Situation #2
+// running file in development for prod
+// through container app
+// no gaurantee of element with id='dev-products'
+// do not want to immediately render the app
+
+export { mount };
